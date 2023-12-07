688c9bcee4bbfed0d5ba24bff62b399b71b1339d
Oscar <om343449@truro-penwith.ac.uk>
2023-12-07 16:03:11 +0000

    add css

--- muppetchat/nav.php
+++ muppetchat/nav.php
@@ -12,12 +12,23 @@ $logged_in = $output[1];
 
 ?>
 
-<nav>
-    <?php if ($logged_in == true): ?>
-        <div><?=$user->get_username();?></div>
-        <div><a href="signout.php">Sign Out</a></div>
-    <?php else: ?>
-        <div><a href="signup.php">Sign Up</a></div>
-        <div><a href="signin.php">Sign In</a></div>
-    <?php endif; ?>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel='stylesheet' href='style.css'>
+</head>
+<body>
+    <nav>
+        <?php if ($logged_in == true): ?>
+            <div><?=$user->get_username();?></div>
+            <div><a href="signout.php">Sign Out</a></div>
+        <?php else: ?>
+            <div><a href="signup.php">Sign Up</a></div>
+            <div><a href="signin.php">Sign In</a></div>
+        <?php endif; ?>
+    </nav>
+</body>
+</html>
